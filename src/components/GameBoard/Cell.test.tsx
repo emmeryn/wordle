3d71ef0b4ca2d06b1b57316108f698de7a9f2a4e
Sue Zheng Hao <szhlibrary@gmail.com>
2022-08-28 17:28:03 +0800

    GameBoard/Cell: Display different colours depending on cellType.

--- src/components/GameBoard/Cell.test.tsx
+++ src/components/GameBoard/Cell.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Cell } from "./Cell";
+import { Cell, CellType } from "./Cell";
 
 test('renders letter when letter is provided', () => {
   const letter = 't';
@@ -13,4 +13,18 @@ test('renders blank cell when no letter is provided', () => {
   const letter = '';
   const { container } = render(<Cell letter={letter}/>);
   expect(container.firstChild).toBeEmptyDOMElement();
+});
+
+describe.each([
+  [CellType.CORRECT, 'bg-green-500'],
+  [CellType.WRONG_POS, 'bg-yellow-500'],
+  [CellType.WRONG_LETTER, 'bg-gray-500'],
+  [CellType.AWAITING_ATTEMPT, 'bg-white-500'],
+])('renders background colour depending on cell type', (cellType, className) => {
+  test(`renders ${CellType[cellType]} cell with ${className}`, () => {
+    const letter = 'w';
+    render(<Cell letter={letter} type={cellType}/>);
+    const letterCell = screen.getByText(letter.toUpperCase());
+    expect(letterCell.classList.contains(className)).toBe(true);
+  })
 });
--- src/components/GameBoard/Cell.tsx
+++ src/components/GameBoard/Cell.tsx
@@ -1,9 +1,31 @@
-type Props = { letter: string };
+type Props = { letter?: string, type?: CellType };
 
-export const Cell = ({ letter }: Props) => {
+export enum CellType {
+  CORRECT,
+  WRONG_POS,
+  WRONG_LETTER,
+  AWAITING_ATTEMPT,
+}
+
+export const Cell = ({ letter, type = CellType.AWAITING_ATTEMPT }: Props) => {
+  let cellClassNames = 'w-14 h-14 rounded-lg shadow-lg items-center justify-center flex';
+  switch (type) {
+    case CellType.CORRECT:
+      cellClassNames += ' bg-green-500';
+      break;
+    case CellType.WRONG_POS:
+      cellClassNames += ' bg-yellow-500';
+      break;
+    case CellType.WRONG_LETTER:
+      cellClassNames += ' bg-gray-500';
+      break;
+    case CellType.AWAITING_ATTEMPT:
+      cellClassNames += ' bg-white-500';
+      break;
+  }
   return (
-    <div className={'w-14 h-14 rounded-lg shadow-lg bg-white-500 items-center justify-center flex'}>
-      {letter.toUpperCase()}
+    <div className={cellClassNames}>
+      {letter?.toUpperCase()}
     </div>
   )
 }
