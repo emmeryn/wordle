bc452c59554ddf883193ad3f4d4414d995fac6d3
Sue Zheng Hao <szhlibrary@gmail.com>
2022-08-29 18:15:22 +0800

    StatsModal: Display WIP modal upon game end.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { GameBoard } from "./components/GameBoard";
 import { Keyboard } from "./components/Keyboard";
+import { StatsModal } from "./components/StatsModal";
 
 function App() {
   const MAX_ATTEMPTS = 6;
@@ -10,6 +11,8 @@ function App() {
   const [attempt, setAttempt] = useState('');
   const [attempts, setAttempts] = useState<string[]>([]);
   const [isGameOver, setIsGameOver] = useState(false)
+  const [isStatsModalOpen, setIsStatsModalOpen] = useState(false);
+
   const [selectedLengthWordList] = useState<string[]>(() => {
     const masterWordList = require('word-list-json');
     const startIdx = masterWordList.lengths[WORD_LENGTH - 1];
@@ -44,9 +47,11 @@ function App() {
     if (attempts.length + 1 >= MAX_ATTEMPTS) {
       alert('Game over! Answer was ' + answer);
       setIsGameOver(true);
+      setIsStatsModalOpen(true);
     } else if (attempt === answer) {
       alert('Correct guess');
       setIsGameOver(true);
+      setIsStatsModalOpen(true);
     } else {
       alert('Wrong guess');
     }
@@ -59,6 +64,7 @@ function App() {
       <header className="App-header">
         <h1>Wordle</h1>
       </header>
+      <StatsModal isOpen={isStatsModalOpen} onClose={() => {setIsStatsModalOpen(false)}}/>
       <GameBoard answer={answer} rowNum={MAX_ATTEMPTS} attempts={attempts} currAttempt={attempt}/>
       <Keyboard answer={answer} attempts={attempts} onLetterPress={onLetterPress} onBackspace={onBackspace} onEnter={onEnter}/>
     </div>
---
+++ src/components/StatsModal/index.tsx
@@ -0,0 +1,35 @@
+import { Dialog } from "@headlessui/react";
+
+type Props = { isOpen: boolean, onClose: () => void };
+
+export const StatsModal = ({ isOpen, onClose }: Props) => {
+  return (
+    <Dialog open={isOpen} onClose={onClose} as={'div'} className={'fixed inset-0 z-10 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center'}>
+      <Dialog.Panel
+        className={'w-full max-w-md inline-block transform overflow-hidden rounded-2xl bg-white p-6 text-center align-middle shadow-xl items-center justify-center'}>
+        <Dialog.Title className={'text-lg font-medium leading-6 text-gray-900'}>
+          Game Over!
+        </Dialog.Title>
+        <Dialog.Description>
+          You've guessed correctly.
+        </Dialog.Description>
+
+        <div className="mt-2">
+          <p className={'text-sm text-gray-500'}>
+          </p>
+        </div>
+
+        <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row justify-center">
+          <button type={'button'}
+                  className={'inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'}
+                  onClick={() => null}>New Game
+          </button>
+          <button type={'button'}
+                  className={'ml-2 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'}
+                  onClick={onClose}>Close
+          </button>
+        </div>
+      </Dialog.Panel>
+    </Dialog>
+  )
+};
